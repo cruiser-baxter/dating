fb91d4b00e9d0880dba0a4223b9290377113bbf3
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-05-09 16:11:48 -0700

    added .htaccess

--- index.php
+++ index.php
@@ -17,5 +17,21 @@ $f3->route('GET /', function (){
     echo $view->render('views/home.html');
 });
 
+//
+// order 1 view
+$f3->route('GET|POST /info', function (){
+    // if form submitted then store data in session array
+    // send user to the next order form
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $_SESSION['food'] = $_POST['food'];
+        $_SESSION['meal'] = $_POST['meal'];
+        header('location: order2');
+    }
+
+    // instantiate a view object
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
+
 // run Fat-Free
 $f3->run();
