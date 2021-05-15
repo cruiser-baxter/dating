c3cf45e3b710dc858f567b4abc42f9f3962f03e9
Cruiser Baxter <cruiser.j.baxter@gmail.com>
2021-05-15 13:19:45 -0700

    completed info.html view that leads to profile.html view

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once ('vendor/autoload.php');
-
+session_start();
 // instantiate Fat-Free
 $f3 = Base::instance();
 
@@ -17,20 +17,51 @@ $f3->route('GET /', function (){
     echo $view->render('views/home.html');
 });
 
-//
-// order 1 view
+// info view
 $f3->route('GET|POST /info', function (){
+    //Reinitialize session array
+    $_SESSION = array();
+
     // if form submitted then store data in session array
-    // send user to the next order form
+    // then send user to the next order form
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $_SESSION['food'] = $_POST['food'];
-        $_SESSION['meal'] = $_POST['meal'];
-        header('location: order2');
+
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['phone'] = $_POST['phone'];
+        $_SESSION['gender'] = $_POST['gender'];
+        header('location: profile');
     }
 
     // instantiate a view object
     $view = new Template();
     echo $view->render('views/info.html');
+});
+
+// profile view
+$f3->route('GET|POST /profile', function (){
+    echo "<h4></h4>";
+    echo "<h4></h4>";
+    echo "<h4></h4>";
+    echo "<h4></h4>";
+    echo "<h4></h4>";
+
+    // if form submitted then store data in session array
+    // then send user to the next order form
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $_SESSION['fname'] = $_POST['fname'];
+        $_SESSION['lname'] = $_POST['lname'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['phone'] = $_POST['phone'];
+        $_SESSION['gender'] = $_POST['gender'];
+        header('location: interests');
+    }
+
+    // instantiate a view object
+    $view = new Template();
+    echo $view->render('views/profile.html');
 });
 
 // run Fat-Free
